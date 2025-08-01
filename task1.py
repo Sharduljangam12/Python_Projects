2257ab713873ead80a0d3d8e80a5de3e96d44141
Sharduljangam12 <152073842+Sharduljangam12@users.noreply.github.com>
2025-08-01 16:11:29 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,24 @@
+#🎯 Task 1: Build a simple CLI script to convert user input text to speech
+import pyttsx3
+
+'''
+input("Enter the Text you want to hear:- ")
+#pyttsx3.speak("I will speak this text")
+engine = pyttsx3.init()
+#engine.say('Sally sells seashells by the seashore.')
+engine.say(input())
+#engine.say('The quick brown fox jumped over the lazy dog.')
+
+engine.runAndWait()'''
+def main():
+    engine = pyttsx3.init()
+    print("Welcome to Tell-Me application")
+    print("This Application will read all the books/Articles/Notes or Anything else you want to read it can read it for you")
+    text = input("Enter the text you want to hear: ")
+    engine.say(text)
+    engine.runAndWait()
+
+    print("Thank you for Using Tell Me")
+
+if __name__ == "__main__":
+    main()
