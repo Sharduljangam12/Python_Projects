cb7d18e4fa4fd84884d90a55fb4df0a91a4189b0
Sharduljangam12 <152073842+Sharduljangam12@users.noreply.github.com>
2025-08-14 15:33:45 +0530

    Update emoji_converter.py

--- emoji_converter.py
+++ emoji_converter.py
@@ -1,7 +1,9 @@
+#This is the project in which whenyou give an emoji it will tell what that emoji is for
 import emoji
 
 giv_txt=input("Enter your string with emoji: ")
 
 t_txt=emoji.demojize(giv_txt)
+
 
 print(t_txt)
